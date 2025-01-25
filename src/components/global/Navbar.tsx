01e3bf2b6e24d9c2ffa02994ecba15a0725d2e89
Vladislav <baiborodov333@mail.ru>
2025-01-25 23:44:58 +0500

    header+main_components_shadcn+aceternity

---
+++ src/components/global/Navbar.tsx
@@ -0,0 +1,61 @@
+import { MenuIcon } from 'lucide-react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+type Props = {};
+
+const Navbar = async (props: Props) => {
+  return (
+    <header className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between">
+      <aside className="flex items-center gap-[2px]">
+        <p className="text-3xl font-bold">Fu</p>
+        <Image
+          src="/fuzzieLogo.png"
+          alt="logo"
+          width={15}
+          height={15}
+          className="shadow-sw"
+        />
+        <p className="text-3xl font-bold">Zie</p>
+      </aside>
+      <nav className="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] hidden md:block">
+        <ul className="flex items-center gap-4 list-none">
+          <li>
+            <Link href="#">Product</Link>
+          </li>
+          <li>
+            <Link href="#">Pricing</Link>
+          </li>
+          <li>
+            <Link href="#">Clients</Link>
+          </li>
+          <li>
+            <Link href="#">Recourses</Link>
+          </li>
+          <li>
+            <Link href="#">Documentation</Link>
+          </li>
+          <li>
+            <Link href="#">Enterprise</Link>
+          </li>
+        </ul>
+      </nav>
+      <aside className="flex items-center gap-4">
+        <Link
+          href="/dashboard"
+        >
+          <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+            {/* добавь сюда юзера */}
+            {true ? "Dashboard" : "Get Started"}
+          </button>
+        </Link>
+  {/* добавь сюда юзера */}
+  
+  <MenuIcon className='md:hidden'/>
+      </aside>
+    </header>
+  );
+};
+
+export default Navbar;
