b30b93eb27cc770407006a49b6e93a5a204491c7
Vladislav <baiborodov333@mail.ru>
2025-01-26 16:33:22 +0500

    dasboard_checkpoint

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,3 +1,5 @@
+
+import Sidebar from '@/components/Sidebar/Sidebar'
 import React from 'react'
 
 type Props = {children: React.ReactNode}
@@ -5,6 +7,7 @@ type Props = {children: React.ReactNode}
 const Layout = (props: Props) => {
   return (
     <div className='flex overflow-hidden h-screen'>
+      <Sidebar/>
 	<div className='w-full'>
 {props.children}
 	</div>
